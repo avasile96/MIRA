377c2ab4089a3429f3dbdb2a71d77cc15639c395
Manuel-O <74353087+Manuel-O@users.noreply.github.com>
2021-10-12 14:00:02 -0500

    up

--- registrationLab/main.m
+++ registrationLab/main.m
@@ -2,8 +2,8 @@
 clear all; close all; clc;
 
 % Read two imges 
-Imoving=im2double(rgb2gray(imread('brain3.png'))); 
-Ifixed=im2double(rgb2gray(imread('brain4.png')));
+Imoving=im2double(rgb2gray(imread('brain1.png'))); 
+Ifixed=im2double(rgb2gray(imread('brain2.png')));
 
 Im=Imoving;
 If=Ifixed;
